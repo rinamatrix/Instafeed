dc6b3bfe4e8b0baf455a004a151a70e3ea7c82d4
Rina Bhabak <53865827+rinamatrix@users.noreply.github.com>
2026-04-17 07:13:25 +0000

    instafeed module upload

--- registration.php
+++ registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Matrixmedia_Instafeed', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Rinadev_Instafeed', __DIR__);
